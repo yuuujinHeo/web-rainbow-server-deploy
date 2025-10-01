d02ce4a6cd9cbb9d2ad573979ff5ae09cf8f7bdd
yujinHeo <yjheo4@rainbow-robotics.com>
2025-10-01 13:14:59 +0900

    F

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 echo "1) nodejs 설치"
 sudo apt update && sudo apt-get install -y curl
-curl -o- <https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh> | bash
+curl -o- <https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh>
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \\. "$NVM_DIR/nvm.sh" 
 [ -s "$NVM_DIR/bash_completion" ] && \\. "$NVM_DIR/bash_completion"  
@@ -22,10 +22,8 @@ sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyring
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" 
+sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
