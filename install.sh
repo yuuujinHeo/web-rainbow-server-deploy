7ede2c3735a556a38c4bce9419f062c9b9bd9416
yujinHeo <yjheo4@rainbow-robotics.com>
2025-10-01 13:17:05 +0900

    F

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 echo "1) nodejs 설치"
 sudo apt update && sudo apt-get install -y curl
-curl -o- <https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh>
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \\. "$NVM_DIR/nvm.sh" 
 [ -s "$NVM_DIR/bash_completion" ] && \\. "$NVM_DIR/bash_completion"  
